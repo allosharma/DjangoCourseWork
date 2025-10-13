45964693bc5bc3a2ff6af0a7c5c8d17242e6ba5f
Alok Sharma <alok.sharma@lauren.co.in>
2025-10-13 10:48:38 +0530

    Added a new app Tracker

---
+++ firstproject/Projects/ExpenseTracker/tracker/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Transaction(BaseModel):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.transaction_type.capitalize()} of {self.amount}"
